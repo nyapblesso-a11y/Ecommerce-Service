563020b803fb66f89c2e9f6624eaa402230efe5d
nyapblesso-a11y <nyapblesso@gmail.com>
2026-02-12 15:19:56 +0100

    the product route has been mounted to the app coure

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ import express  from 'express' ;
 import path  from 'path';
 import cookieParser from 'cookie-parser';
 import logger  from 'morgan';
-
 import indexRouter  from './routes/index.js';
-import usersRouter  from './routes/users.js';
+import productRouter from './routes/product.js'
 
 const app = express();
 
@@ -14,9 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/product', productRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
